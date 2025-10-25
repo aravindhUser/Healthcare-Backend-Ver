b87a8fb345f30eb8e754f73ef4498aae20389da2
Amal Krishna <amalkrishna.k@cognizant.com>
2025-10-25 22:27:13 +0530

    updated
    Updated ConsultationDTO to include appointmentId, doctor, and patient fields. Removed commented-out code and adjusted data types.

--- Consultations/Consultations/src/main/java/com/healthcare/Consultations/dto/ConsultationDTO.java
+++ Consultations/Consultations/src/main/java/com/healthcare/Consultations/dto/ConsultationDTO.java
@@ -1,49 +1,33 @@
 package com.healthcare.Consultations.dto;
 
-
-import lombok.*;
-
-import java.time.LocalDate;
-import java.util.List;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Data 
-public class ConsultationDTO {
-
-//   private String appointmentId;
-   private String patientId;
-   private String patientName;
-   private Long doctorId;
-   private String doctorName;
-   private LocalDate date;
-   private String notes;
-   
-   
-   private List<PrescriptionDTO> prescriptions;
-   @NoArgsConstructor
-   @AllArgsConstructor
-   @Builder
-   @Data
-   public static class PrescriptionDTO {
-       private String medicineName;
-       private String dose;
-       private String frequency;
-       private String duration;
-   }
-   
-   
-//   private List<AttachmentDTO> attachments;
-//
-//   @NoArgsConstructor
-//   @AllArgsConstructor
-//   @Builder
-//   @Data
-//   public static class AttachmentDTO {
-//       private String filename;
-//       private long size;
-//       private String mimeType;
-//   }
-}
-
+import lombok.*;  
+import java.time.LocalDate;  
+import java.util.List;  
+  
+@Data  
+@NoArgsConstructor  
+@AllArgsConstructor  
+@Builder  
+public class ConsultationDTO {  
+    private int appointmentId;  
+    private int doctorId;
+    private int patientId;
+    private String doctorName;  
+    private String patientName; 
+    private DoctorDTO doctor;  
+    private PatientDTO patient;  
+    private LocalDate date;  
+    private String notes;  
+    private List<PrescriptionDTO> prescriptions;  
+  
+    @Data  
+    @NoArgsConstructor  
+    @AllArgsConstructor  
+    @Builder  
+    public static class PrescriptionDTO {  
+        private String medicineName;  
+        private String dose;  
+        private String frequency;  
+        private String duration;  
+    }  
+}  
